17f7f8618b6e30e3f62391b02dc8d7caac553bf6
Milad Vazan <165850937+milad-ai@users.noreply.github.com>
2024-04-20 14:04:10 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.write('Deep Learning Researcher')
+st.title("Deep Learning Researcher")
+st.header("Milad Vazan")
+st.markdown("Milad Vazan is a dedicated deep learning researcher and author, specializing in Soft Computing and Artificial Intelligence. With a Bachelor’s Degree in Computer Engineering (Software) and a Master’s Degree in Computer Science, his research efforts are focused on developing robust neural network architectures and efficient training algorithms. Milad has also authored valuable books in Persian on deep learning and machine learning, catering to the needs of Persian-speaking readers.")
+
